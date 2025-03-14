57ae7def2d34e2ef003767bc66230bf1614e7f22
Lenhi26JJ <lenhi.260807@gmail.com>
2025-03-14 08:07:57 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function login()
+{
+  let u = username.value
+  let p = tự đặt.value
+  if(u=="nhi" && p=="baonhi")
+  {
+    window.open("home.html")
+    document getElementById(compiler).style.display="block"
+  }
+}
